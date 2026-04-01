d6991fb5f22ac44dc046762c9e9a81e87604622c
Sat R <76838463+Sat-14@users.noreply.github.com>
2026-04-01 14:42:19 +0530

    Time: 336 ms (5.03%) | Memory: 19.3 MB (50.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,25 +1,20 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        n=len(s)
-        c=set()
-        left=0
-        maxl=0
-        for right in range(n):
-            if s[right]  not in c:
-                c.add(s[right])
-                maxl=max(right-left+1,maxl)
-            else:
-                while s[right] in c:
-                 c.remove(s[left])
-                 left+=1
-                c.add(s[right]) 
-                maxl=max(right-left+1,maxl)
-        return maxl        
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+    
+        a=len(s)
+        maxx=0
+        for i in range(a):
+            fast=i
+            m={}
+            for fast in range(i,a):
+                
+                
+                if s[fast] in m:
+                    
+                    break
 
-
-        
+                m[s[fast]]=1
+                maxx=max(maxx,fast-i+1) 
+        return maxx            
+                
 
